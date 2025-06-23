285da6cb29f0f2b5dc20c9a34bd9e3c75ee20e79
LuizMenegon <luiz.fernandomenegon@hotmail.com>
2025-06-23 19:38:18 -0300

    exemplos da aula 1

---
+++ tupe-boolean.js
@@ -0,0 +1,6 @@
+const primeiroNumero = 10;
+const segundoNumero = 20;
+
+console.log("O valor é:" + primeiroNumero === segundoNumero); // Saída: false
+console.log(primeiroNumero !== segundoNumero); // Saída: true
+console.log(primeiroNumero < segundoNumero); // Saída: true
---
+++ type-numbers.js
@@ -0,0 +1,20 @@
+//Tipo Number
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const soma = primeiroNumero + segundoNumero;
+console.log(soma); // Saída: 3
+
+//Ponto flutuante
+const numeroPontoFlutuante = 3.3;
+
+const numeroPontoFlutuanteSemZero = .5;
+
+const novaSoma = primeiroNumero + numeroPontoFlutuante;
+console.log(novaSoma); // Saída: 4.3
+
+const alura = "Alura";
+console.log(alura * primeiroNumero); // Saída: NaN (Not a Number)
+
